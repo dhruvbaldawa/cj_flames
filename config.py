eb58362d6c24d17c1b60c687043eec06dcb99adf
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-07-21 16:29:48 +0530

    Removing config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-BASE_URL = 'https://graph.facebook.com/'
-REQUEST_TOKEN_URL = None
-ACCESS_TOKEN_URL = '/oauth/access_token'
-AUTHORIZE_URL = 'https://www.facebook.com/dialog/oauth'
-CONSUMER_KEY = '' # Facebook Application ID
-CONSUMER_SECRET = '' # Facebook Application Secret
