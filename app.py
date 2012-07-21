e66f70940e1dee08d13c046c107c1bd4871de3e0
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-07-21 15:47:51 +0530

    Working with the Facebook API

--- app.py
+++ app.py
@@ -1,9 +1,50 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, redirect, url_for, session, request
+from flaskext.oauth import OAuth
+from config import *
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+app.secret_key = 'abcdeghji'
+app.debug = True
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url=BASE_URL,
+    request_token_url=REQUEST_TOKEN_URL,
+    access_token_url=ACCESS_TOKEN_URL,
+    authorize_url=AUTHORIZE_URL,
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_KEY,
+    request_token_params={'scope': 'email',
+                          'response_type': 'token',  },
+)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
 
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -2,5 +2,5 @@ BASE_URL = 'https://graph.facebook.com/'
 REQUEST_TOKEN_URL = None
 ACCESS_TOKEN_URL = '/oauth/access_token'
 AUTHORIZE_URL = 'https://www.facebook.com/dialog/oauth'
-CONSUMER_KEY = '' # Facebook Application ID
-CONSUMER_SECRET = '' # Facebook Application Secret
+CONSUMER_KEY = '475277795834128' # Facebook Application ID
+CONSUMER_SECRET = 'e03665684dd623b6922b7b9b2f3eec2f' # Facebook Application Secret
