2f5e6f5c5b7f0f6eff93bfd1c98ea6e5756b7849
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-07-21 16:30:58 +0530

    Adding config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BASE_URL = 'https://graph.facebook.com/'
+REQUEST_TOKEN_URL = None
+ACCESS_TOKEN_URL = '/oauth/access_token'
+AUTHORIZE_URL = 'https://www.facebook.com/dialog/oauth'
+CONSUMER_KEY = '' # Facebook Application ID
+CONSUMER_SECRET = '' # Facebook Application Secret
